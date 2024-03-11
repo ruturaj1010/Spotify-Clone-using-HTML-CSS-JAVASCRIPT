46c1f0898be4faed89d84d4335f8b49d50a31875
Ruturaj <ruturajnikam1010@gmail.com>
2024-03-12 01:55:24 +0530

    changes updated 	deleted:    Songs/Dandelions_by_Ruth_B____Lyrics____re-upload____%21%21not_my_sound%21%21(128k).m4a 	deleted:    Songs/Imagine Dragons - Believer (Lyrics)(M4A_128K).m4a 	deleted:    Songs/Justin_Bieber_-_Intentions_(Official_Video_(Short_Version))_ft._Quavo(128k).m4a 	deleted:    Songs/Perfect_-_Ed_Sheeran_(Lyrics)(128k).m4a 	deleted:    Songs/Pink_Sweat$_-_At_My_Worst_(Lyrics)(128k).m4a 	deleted:    Songs/Taylor Swift - Lover (Official Music Video)(M4A_128K).m4a 	deleted:    Songs/Taylor_Swift_-_Love_Story_(Lyrics)_romeo_save_me(128k).m4a 	modified:   Time.txt 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("let's start javascript");
+console.log( "let's start javascript" );
+
+
+async function getSongs () {
+    let a = await fetch( "http://127.0.0.1:3000/Songs/" );
+    let response = await a.text();
+    console.log( response );
+
+    let div = document.createElement( "div" );
+    div.innerHTML = response;
+    let as = div.getElementsByTagName( "a" );
+
+    let songs = []
+
+    for ( let index = 0; index < as.length; index++ ) {
+        const element = as[index];
+        if ( element.href.endsWith( ".mp3" )) {
+            songs.push(element.href.split("/Songs/")[1])
+        }
+    }
+    return songs;
+}
+
+async function main () {
+    // Get the list of songs
+    let songs = await getSongs();
+    console.log( songs );
+
+    let songul = document.querySelector(".song-container").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songul.innerHTML = songul.innerHTML + `<li> ${song.replaceAll("%20"," ")}</li>`;
+    }
+    // Play the first song
+    var audio = new Audio(songs[1]);
+    audio.play();
+}
+
+main()
