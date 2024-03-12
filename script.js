ee794173e208627a8b2382ad1de881dda4ee394d
Ruturaj <ruturajnikam1010@gmail.com>
2024-03-12 12:29:53 +0530

    updated 	new file:   Resources/music.svg 	modified:   Resources/play.svg 	new file:   Songs/Justin Bieber - Baby ft. Ludacris(MP3_160K).mp3 	new file:   Songs/One Direction - Night Changes(MP3_160K).mp3 	new file:   Songs/Taylor Swift - Love Story(MP3_160K).mp3 	new file:   Songs/Taylor Swift - Lover(MP3_160K).mp3 	modified:   Time.txt 	renamed:    faviconspotify.ico -> favicon.ico 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -14,25 +14,44 @@ async function getSongs () {
 
     for ( let index = 0; index < as.length; index++ ) {
         const element = as[index];
-        if ( element.href.endsWith( ".mp3" )) {
-            songs.push(element.href.split("/Songs/")[1])
+        if ( element.href.endsWith( ".mp3" ) ) {
+            songs.push( element.href.split( "/Songs/" )[1] )
         }
     }
     return songs;
 }
 
+const playMusic = (track)=> {
+    let audio = new Audio("/Songs/" + track)
+    audio.play()
+}
+
 async function main () {
+    let currentsong = new Audio();
     // Get the list of songs
     let songs = await getSongs();
     console.log( songs );
 
-    let songul = document.querySelector(".song-container").getElementsByTagName("ul")[0]
-    for (const song of songs) {
-        songul.innerHTML = songul.innerHTML + `<li> ${song.replaceAll("%20"," ")}</li>`;
+    // Show all the songs in playlist
+    let songul = document.querySelector( ".song-container" ).getElementsByTagName( "ul" )[0]
+    for ( const song of songs ) {
+        songul.innerHTML = songul.innerHTML + `<li> <img class="invert" src="Resources/music.svg" alt="">
+        <div class="music-info">
+            <div>${song.replaceAll( "%20", " " )}</div>
+            <div>Ruturaj Nikam</div>
+        </div>
+        <div class="play-now">
+            <span>Play Now</span>
+            <img class="invert" src="Resources/play.svg" alt="">
+        </div></li>`;
     }
-    // Play the first song
-    var audio = new Audio(songs[1]);
-    audio.play();
+
+    Array.from(document.querySelector(".song-list").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click" , element=>{
+            console.log(e.querySelector(".music-info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".music-info").firstElementChild.innerHTML.trim())
+        })
+    })
 }
 
 main()
