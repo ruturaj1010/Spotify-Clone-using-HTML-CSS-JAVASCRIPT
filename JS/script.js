35d3b69bca5905308a8fb931f2e33ee06ae7fc94
Ruturaj Nikam <ruturajnikam1010@gmail.com>
2024-08-05 18:19:32 +0530

    modified:   JS/script.js

--- JS/script.js
+++ JS/script.js
@@ -81,43 +81,44 @@ function formatTime ( seconds ) {
 async function DisplayAlbums () {
     let a = await fetch( "http://127.0.0.1:3000/songs/" );
     let response = await a.text();
-    
+    // console.log(response)
+
     let div = document.createElement( "div" );
     div.innerHTML = response;
     let anchors = div.getElementsByTagName( "a" )
     let array = Array.from( anchors )
-        for (let index = 0; index < array.length; index++) {
-            const e = array[index];
-            
-        if ( e.href.includes( "/songs" ) && !e.href.includes(".htaccess") ) {
+    for ( let index = 0; index < array.length; index++ ) {
+        const e = array[index];
+
+        if ( e.href.includes( "/songs" ) && !e.href.includes( ".htaccess" ) ) {
             let folder = e.href.split( "/" ).slice( -2 )[0]
             // get the metadata of the folder
             let a = await fetch( `http://127.0.0.1:3000/songs/${folder}/info.json` );
             let response = await a.json();
             // console.log(response)
-            let cardContainer = document.querySelector(".cardContainer")
-            cardContainer.innerHTML = cardContainer.innerHTML  + `<div data-folder="${folder}" class="card">
-            <div class="playbtn">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
-                    color="#000000" fill="none">
-                    <path
-                        d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z"
-                        stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" fill="#000" />
-                </svg>
-            </div>
-            <img src="/songs/${folder}/cover.jpeg" alt="This is an img">
-            <h3>${response.title}</h3>
-            <p>${response.description}</p>
-        </div>`
+            let cardContainer = document.querySelector( ".cardContainer" )
+            cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
+                <div class="playbtn">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                        color="#000000" fill="none">
+                        <path
+                            d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z"
+                            stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" fill="#000" />
+                    </svg>
+                </div>
+                <img src="/songs/${folder}/cover.jpeg" alt="This is an img">
+                <h3>${response.title}</h3>
+                <p>${response.description}</p>
+                </div>`
         }
     }
-    
+
     // load the playlist whenever the card is clicked
     Array.from( document.getElementsByClassName( "card" ) ).forEach( e => {
-        // console.log(e)
+        console.log(e)
         e.addEventListener( "click", async item => {
             songs = await getSongs( `songs/${item.currentTarget.dataset.folder}` );
-            playMusic(songs[0])
+            playMusic( songs[0] )
         } )
     } );
 
@@ -183,22 +184,22 @@ async function main () {
     document.querySelector( ".range" ).getElementsByTagName( "input" )[0].addEventListener( "change", ( e ) => {
         // console.log( e, e.target, e.target.value , "/ 100" )
         currentsong.volume = parseInt( e.target.value ) / 100
-        if ( currentsong.volume > 0 ){
-            document.querySelector(".songvolimg>img").src = document.querySelector(".songvolimg>img").src.replace("mute.svg","volume.svg")
+        if ( currentsong.volume > 0 ) {
+            document.querySelector( ".songvolimg>img" ).src = document.querySelector( ".songvolimg>img" ).src.replace( "mute.svg", "volume.svg" )
         }
     } )
 
-    document.querySelector(".songvolimg>img").addEventListener("click" , e=>{
-        if(e.target.src.includes("volume.svg")){
-            e.target.src = e.target.src.replace("volume.svg","mute.svg")
+    document.querySelector( ".songvolimg > img" ).addEventListener( "click", e => {
+        if ( e.target.src.includes( "volume.svg" ) ) {
+            e.target.src = e.target.src.replace( "volume.svg", "mute.svg" )
             currentsong.volume = 0;
             document.querySelector( ".range" ).getElementsByTagName( "input" )[0].value = 0;
         } else {
-            e.target.src = e.target.src.replace("mute.svg" ,"volume.svg")
+            e.target.src = e.target.src.replace( "mute.svg", "volume.svg" )
             currentsong.volume = 0.5;
             document.querySelector( ".range" ).getElementsByTagName( "input" )[0].value = 40;
         }
-    })
+    } )
 
 }
 
